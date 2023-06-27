ee0db42b13b34533069ad8105bfb9f8cb9f5d337
gioboa <giorgiob.boa@gmail.com>
2023-06-27 17:23:07 +0200

    copy insights website

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,7 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
+import { Insights } from '@builder.io/qwik-labs';
 
 import './global.css';
 
@@ -9,7 +10,7 @@ export default component$(() => {
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
    *
-   * Dont remove the `<head>` and `<body>` elements.
+   * Don't remove the `<head>` and `<body>` elements.
    */
 
   return (
@@ -18,6 +19,10 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
+        <Insights
+          publicApiKey={import.meta.env.PUBLIC_QWIK_INSIGHTS_KEY}
+          postUrl="/api/v1/${publicApiKey}/post/"
+        />
       </head>
       <body lang="en">
         <RouterOutlet />
