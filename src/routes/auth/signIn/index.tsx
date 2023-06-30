23f5f363cd7874859dfebee8f527b5f5607cd22f
gioboa <giorgiob.boa@gmail.com>
2023-06-30 15:10:53 +0200

    add GitHub auth

--- src/routes/auth/signIn/index.tsx
+++ src/routes/auth/signIn/index.tsx
@@ -1,64 +1,87 @@
-import { component$ } from "@builder.io/qwik";
-import { Form, globalAction$, z, zod$ } from "@builder.io/qwik-city";
-import { paths } from "~/routes/layout";
-import { updateAuthCookies } from "~/server/auth/auth";
-import { createSupabase } from "~/server/auth/supabase";
-import { useAnonymousRoute } from "../layout";
+import { component$ } from '@builder.io/qwik';
+import { Form, globalAction$, z, zod$ } from '@builder.io/qwik-city';
+import { paths } from '~/routes/layout';
+import { updateAuthCookies } from '~/server/auth/auth';
+import { createSupabase } from '~/server/auth/supabase';
+import { useAnonymousRoute } from '../layout';
 
 export const useSignInPasswordAction = globalAction$(
-  async (data, event) => {
-    const supabase = createSupabase(event);
+	async (data, event) => {
+		const supabase = createSupabase(event);
 
-    const result = await supabase.auth.signInWithPassword(data);
+		const result = await supabase.auth.signInWithPassword(data);
 
-    if (result.error || !result.data.session) {
-      const status = result.error?.status || 400;
-      return event.fail(status, {
-        formErrors: [result.error?.message],
-      });
-    }
+		if (result.error || !result.data.session) {
+			const status = result.error?.status || 400;
+			return event.fail(status, {
+				formErrors: [result.error?.message],
+			});
+		}
 
-    updateAuthCookies(event, result.data.session);
+		updateAuthCookies(event, result.data.session);
 
-    throw event.redirect(302, paths.index);
-  },
-  zod$({
-    email: z.string().email(),
-    password: z.string(),
-  })
+		throw event.redirect(302, paths.index);
+	},
+	zod$({
+		email: z.string().email(),
+		password: z.string(),
+	})
 );
 
+export const useGitHubAction = globalAction$(async (_, event) => {
+	const supabase = createSupabase(event);
+
+	const response = await supabase.auth.signInWithOAuth({
+		provider: 'github',
+	});
+	return {
+		success: true,
+		url: response.data.url || '',
+	};
+});
+
 export default component$(() => {
-  useAnonymousRoute();
-  const action = useSignInPasswordAction();
+	useAnonymousRoute();
+	const action = useSignInPasswordAction();
+	const gitHubAction = useGitHubAction();
 
-  return (
-    <div>
-      <h1>Sign In</h1>
-      <div>
-        <Form action={action}>
-          <h2>Sign in with password</h2>
+	return (
+		<div>
+			<h1>Sign In</h1>
+			<div>
+				<Form action={action}>
+					<h2>Sign in with password</h2>
 
-          <div>
+					<div>
             <label for="email">
-              <span>Email</span>
-            </label>
+							<span>Email</span>
+						</label>
             <input id="email" placeholder="Email" name="email" type="email" />
-            <span>{action.value?.fieldErrors?.email?.[0]}</span>
-          </div>
+						<span>{action.value?.fieldErrors?.email?.[0]}</span>
+					</div>
 
-          <div>
+					<div>
             <label for="password">
-              <span>Password</span>
-            </label>
+							<span>Password</span>
+						</label>
             <input id="password" name="password" type="password" />
-            <span>{action.value?.fieldErrors?.password?.[0]}</span>
-          </div>
+						<span>{action.value?.fieldErrors?.password?.[0]}</span>
+					</div>
 
-          <span>{action.value?.formErrors?.[0]}</span>
+					<span>{action.value?.formErrors?.[0]}</span>
           <button type="submit">Sign In</button>
-        </Form>
-      </div>
-    </div>
-  );
+				</Form>
+			</div>
+			<div>
+				<button
+					onClick$={async () => {
+						const { value } = await gitHubAction.submit();
+						window.open(value.url, '_self');
+					}}
+				>
+					GitHub
+				</button>
+			</div>
+		</div>
+	);
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,86 @@
-import { component$ } from "@builder.io/qwik";
-import { Link, routeLoader$ } from "@builder.io/qwik-city";
-import { getUserFromEvent } from "~/server/auth/auth";
-import { paths } from "./layout";
+import { component$, useVisibleTask$ } from '@builder.io/qwik';
+import {
+  Link,
+  globalAction$,
+  routeLoader$,
+  useNavigate,
+  z,
+  zod$,
+} from '@builder.io/qwik-city';
+import { getUserFromEvent, updateAuthCookies } from '~/server/auth/auth';
+import { paths } from './layout';
 
 export const useUser = routeLoader$((event) => {
-  return getUserFromEvent(event);
+	return getUserFromEvent(event);
 });
 
-export default component$(() => {
-  const userSig = useUser();
+export const useSetSessionAction = globalAction$(
+	(data, event) => {
+		updateAuthCookies(event, data);
+	},
+	zod$({
+		access_token: z.string(),
+		expires_in: z.coerce.number(),
+		refresh_token: z.string(),
+	})
+);
 
-  return (
-    <div>
-      Future home of Qwik Insights! {userSig.value?.email}
-      {userSig.value?.email ? (
-        <ul>
-          <li>
-            <Link href={paths.dashboard}>Dashboard</Link>
-          </li>
-          <li>
-            <Link href={paths.signOut}>Sign Out</Link>
-          </li>
-        </ul>
-      ) : (
-        <ul>
-          <li>
-            <Link href={paths.signIn}>Sign In</Link>
-          </li>
-          <li>
-            <Link href={paths.signUp}>Sign Up</Link>
-          </li>
-        </ul>
-      )}
-    </div>
-  );
+export default component$(() => {
+	const userSig = useUser();
+	const navigate = useNavigate();
+	const action = useSetSessionAction();
+
+	useVisibleTask$(async () => {
+		const hash = window.location.hash.substring(1);
+
+		if (!hash) {
+			return;
+		}
+
+		const params = new URLSearchParams(hash);
+		const access_token = params.get('access_token');
+		const expires_in = params.get('expires_in');
+		const refresh_token = params.get('refresh_token');
+
+		if (!access_token || !expires_in || !refresh_token) {
+			return;
+		}
+
+		await action.submit({
+			access_token,
+			expires_in: +expires_in,
+			refresh_token,
+		});
+
+		if (action.value?.failed) {
+			return;
+		}
+
+		navigate(paths.index);
+	});
+
+	return (
+		<div>
+			Future home of Qwik Insights! {userSig.value?.email}
+			{userSig.value?.email ? (
+				<ul>
+					<li>
+						<Link href={paths.dashboard}>Dashboard</Link>
+					</li>
+					<li>
+						<Link href={paths.signOut}>Sign Out</Link>
+					</li>
+				</ul>
+			) : (
+				<ul>
+					<li>
+						<Link href={paths.signIn}>Sign In</Link>
+					</li>
+					<li>
+						<Link href={paths.signUp}>Sign Up</Link>
+					</li>
+				</ul>
+			)}
+		</div>
+	);
 });
